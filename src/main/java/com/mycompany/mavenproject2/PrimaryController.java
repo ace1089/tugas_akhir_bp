1910dcb1d859ca9539ff356ff7f99dbf27f80440
hanifrasendra <133474723+hanifrasendra@users.noreply.github.com>
2024-10-18 19:27:26 +0700

    Update PrimaryController.java

--- src/main/java/com/mycompany/mavenproject2/PrimaryController.java
+++ src/main/java/com/mycompany/mavenproject2/PrimaryController.java
@@ -1,1 +1,12 @@
+package com.mycompany.mavenproject2;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+
+    @FXML
+    private void Primary() throws IOException {
+    }
+}
 
