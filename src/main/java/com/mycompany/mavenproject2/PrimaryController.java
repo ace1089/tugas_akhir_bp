3d3dc6d7359f41e9e11d15b53b4f4fd681bd96b7
hanifrasendra <133474723+hanifrasendra@users.noreply.github.com>
2024-10-24 16:48:30 +0700

    Update PrimaryController.java

--- src/main/java/com/mycompany/mavenproject2/PrimaryController.java
+++ src/main/java/com/mycompany/mavenproject2/PrimaryController.java
@@ -1,12 +1,83 @@
 package com.mycompany.mavenproject2;
 
 import java.io.IOException;
+import javafx.event.ActionEvent;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class PrimaryController {
-
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+    
     @FXML
-    private void Primary() throws IOException {
+    private void addNew(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("todaylist.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    private void Home(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("primary.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+     @FXML
+    void Addnew(ActionEvent event) throws Exception {
+        Addnewpage();
+    }
+    double x,y = 0;
+    public void Addnewpage()throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("addnew.fxml"));
+        Stage stage = new Stage();
+
+        stage.initStyle(StageStyle.UNDECORATED);
+        root.setOnMousePressed(event -> {
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
+
+        //move around here
+        root.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX() - x);
+            stage.setY(event.getScreenY() - y);
+        });
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
+    @FXML
+    private Button exitButton;
+    @FXML
+    private StackPane scenePane;
+    
+            
+    public void exit(ActionEvent event) {
+        Alert alert = new Alert(AlertType.CONFIRMATION);
+        alert.setTitle("Exit");
+        alert.setHeaderText("You're about to logout");
+        alert.setContentText("Do you want to save before exiting");
+        
+        if(alert.showAndWait().get() == ButtonType.OK){
+            stage = (Stage) scenePane.getScene().getWindow();
+            System.out.println("");
+            stage.close();
+        }
     }
 }
+
 
